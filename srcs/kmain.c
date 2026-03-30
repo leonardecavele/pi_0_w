00fffa2dbbe77fe7cc7b9234ea1c4576678254da
leonardecavele <leonard.decavele@gmail.com>
2026-03-30 16:03:40 +0200

    wip: interrupts

--- srcs/kmain.c
+++ srcs/kmain.c
@@ -28,15 +28,6 @@ int kmain(uintptr_t dtb)
 	gpio_set_func(27u, GPIO_OUTPUT);
 	gpio_set_func(22u, GPIO_OUTPUT);
 
-	/* enable bcm2835 IRQ */
-	REG4B(BCM2835_IRQ + IRQ_ENABLE_2) = IRQ_GPIO_BANK0_BIT;
-
-	/* set-up button GPIO */
-	gpio_set_func(23u, GPIO_INPUT);
-	gpio_set_pull(23u, GPIO_PULL_UP);
-	gpio_event_clear(23u);
-	gpio_enable_falling(23u);
-
 	/* set-up UART0 GPIO */
 	gpio_set_func(14u, GPIO_ALT0);
 	gpio_set_pull(14u, GPIO_PULL_OFF);
@@ -44,11 +35,18 @@ int kmain(uintptr_t dtb)
 	gpio_set_pull(15u, GPIO_PULL_OFF);
 	/* init UART0 */
 	uart_init(BCM2835_UART0, UART_CLK, UART_BAUD);
-
 	/* UART test */
 	uart_printf(BCM2835_UART0, "hello world\r\n");
 	//wait_cmd(BCM2835_UART0);
 
+	/* set-up button GPIO */
+	gpio_set_func(23u, GPIO_INPUT);
+	gpio_set_pull(23u, GPIO_PULL_UP);
+	gpio_event_clear(23u);
+	gpio_enable_falling(23u);
+	gpio_event_clear(23u);
+	/* enable bcm2835 IRQ */
+	REG4B(BCM2835_IRQ + IRQ_ENABLE_2) = IRQ_GPIO_BANK0_BIT;
 	/* enable interrupts */
 	asm volatile("cpsie i");
 
